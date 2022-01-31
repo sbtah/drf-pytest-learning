de4b534d1fb881705de3ea3ce39ed1f0724b5626
Sbtah <shisastore@protonmail.com>
2022-01-31 21:21:25 +0100

    Added models for Student and Classroom

--- classroom/models.py
+++ classroom/models.py
@@ -1,12 +1,35 @@
 from django.db import models
 
 
+class Student(models.Model):
+    """Class for Student object."""
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    user_number = models.IntegerField(unique=True)
+    is_qualified = models.BooleanField(default=False)
+    average_score = models.FloatField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def get_grade(self):
+        if self.grade < 40:
+            return 'Fail'
+        elif self.grade < 70 > 40:
+            return 'Pass'
+        elif self.grade < 100 > 70:
+            return 'Excellent'
+        else:
+            return 'Error'
+
+
 class Classroom(models.Model):
-    """Class for Classroom object"""
+    """Class for Classroom object."""
 
     name = models.CharField(max_length=120)
     student_capacity = models.IntegerField()
-    students = models.ManyToManyField('Test')
+    students = models.ManyToManyField(Student)
 
     def __str__(self):
         return self.name
