cb96ec8a1e15c1bc65b80bacc4a1f9aee5adbbdc
Sbtah <shisastore@protonmail.com>
2022-02-02 20:34:09 +0100

    Modified tests to pytest style.

--- core/tests/test_pytest_models.py
+++ core/tests/test_pytest_models.py
@@ -1,51 +1,70 @@
 from django.test import TestCase
-from classroom.models import Student
+from classroom.models import Classroom, Student
 from mixer.backend.django import mixer
+import pytest
+pytestmark = pytest.mark.django_db
 
 
-class TestStudentModel(TestCase):
-    """Test case for Student object."""
+def test_student_can_be_created():
+    """Test that Student object can be created."""
 
-    # def setUp(self):
-    #
-    #     self.student = Student.objects.create(
-    #         first_name='test',
-    #         last_name='test',
-    #         user_number=1,
-    #     )
+    student1 = mixer.blend(Student, first_name='test')
+    assert student1.first_name == 'test'
 
-    def test_student_can_be_created(self):
-        """Test that Student object can be created."""
 
-        student1 = mixer.blend(Student, first_name='test')
-        assert student1.first_name == 'test'
+def test_str_of_student():
+    """Test __str__ of Student model."""
 
-    def test_str_of_student(self):
-        """Test __str__ of Student model."""
+    student1 = mixer.blend(Student, first_name='test', last_name='test')
+    assert str(student1) == 'test test'
 
-        student1 = mixer.blend(Student, first_name='test', last_name='test')
-        assert str(student1) == 'test test'
 
-    def test_grade_fail(self):
-        """Test get_grade returns proper data
-        (average_score < 40) -> 'Fail'
-        """
+def test_grade_fail():
+    """Test get_grade returns proper data
+    (average_score < 40) -> 'Fail'
+    """
 
-        student1 = mixer.blend(Student, average_score=33)
-        assert student1.get_grade() == 'Fail'
+    student1 = mixer.blend(Student, average_score=33)
+    assert student1.get_grade() == 'Fail'
 
-    def test_grade_pass(self):
-        """Test get_grade returns proper data
-        (average_score < 70 and average_score > 40) -> 'Pass'
-        """
 
-        student1 = mixer.blend(Student, average_score=66)
-        assert student1.get_grade() == 'Pass'
+def test_grade_pass():
+    """Test get_grade returns proper data
+    (average_score < 70 and average_score > 40) -> 'Pass'
+    """
 
-    def test_grade_excellent(self):
-        """Test get_grade returns proper data
-        (average_score < 100 and average_score > 70) -> 'Excellent'
-        """
+    student1 = mixer.blend(Student, average_score=66)
+    assert student1.get_grade() == 'Pass'
 
-        student1 = mixer.blend(Student, average_score=86)
-        assert student1.get_grade() == 'Excellent'
+
+def test_grade_excellent():
+    """Test get_grade returns proper data
+    (average_score < 100 and average_score > 70) -> 'Excellent'
+    """
+
+    student1 = mixer.blend(Student, average_score=86)
+    assert student1.get_grade() == 'Excellent'
+
+
+def test_grade_error():
+    """Test get_grade returns proper data
+    (average_score > 100) -> 'Error'
+    """
+
+    student1 = mixer.blend(Student, average_score=101)
+    assert student1.get_grade() == 'Error'
+
+
+class TestStudentModel():
+    """"""
+    pass
+
+
+class TestClassroomModel:
+    """"""
+
+    def test_classroom_str(self):
+        """"""
+
+        classroom = mixer.blend(Classroom, name='test classroom')
+        assert str(classroom) == 'test classroom'
